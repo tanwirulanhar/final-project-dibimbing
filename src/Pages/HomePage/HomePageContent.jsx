82c6ffca006ae479b572baf859a4babda83251bc
tanwirul anhar <mohammad.tanwirul29@gmail.com>
2024-07-27 23:21:42 +0800

    menambahkan Link Navigasi

--- src/Pages/HomePage/HomePageContent.jsx
+++ src/Pages/HomePage/HomePageContent.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Button from "../../Component/Element/Button/Button";
 import vector2 from "../../assets/Group-1.png";
-
+import { Link } from "react-router-dom";
 
 const HomePageContent = () => {
   return (
     <div className="relative flex justify-between min-h-screen bg-white">
-        <img src={vector2} alt="#" className="absolute top-0 z-0 -left-10" />
+      <img src={vector2} alt="#" className="absolute top-0 z-0 -left-10" />
 
       <div className="z-10 pl-20 mt-44 ">
         <h1 className="mb-4 text-4xl font-bold text-green-600">
@@ -15,13 +15,14 @@ const HomePageContent = () => {
         </h1>
         <p className="pb-10 text-lg text-green-800 w-541">
           Take a step beyond the ordinary and explore the unseen beauty that
-          lies beyond the horizon. <br/>Every moment is a new adventure
-          waiting to unfold.
+          lies beyond the horizon. <br />
+          Every moment is a new adventure waiting to unfold.
         </p>
-        <Button text="Explore" className="justify-center" />
+        <Link to={"/promoUser"}>
+          {" "}
+          <Button text="Explore" className="justify-center" />
+        </Link>
       </div>
-
-
     </div>
   );
 };
